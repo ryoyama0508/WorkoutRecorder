132c9f3b4d39811e60f23c96dba426e4d7b567a9
山本凌 <ryo.dr.dre0508@gmail.com>
2020-04-21 01:25:27 +0900

    sign up file created

--- sql.go
+++ sql.go
@@ -8,14 +8,20 @@ import (
 )
 
 func main() {
-	fmt.Println("start database")
-
-	db, err := sql.Open("mysql", "")
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/w_recorder")
 	if err != nil {
 		panic(err)
 	}
 
+	fmt.Println("start database")
+
 	defer db.Close()
+
+	insert, err := db.Query("INSERT INTO users (email, hashed_password, name) VALUES ('test','test','ryo')")
+	if err != nil {
+		panic(err)
+	}
+	defer insert.Close()
 
 	fmt.Println("success")
 }
