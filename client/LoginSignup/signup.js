dfb9f0b3e79465b5d0d3a6062e673a730f81e9fa
山本凌 <ryo.dr.dre0508@gmail.com>
2020-04-21 22:24:12 +0900

    url

--- client/LoginSignup/signup.js
+++ client/LoginSignup/signup.js
@@ -4,13 +4,12 @@ function httpGetAsync(theUrl, callback) {
 
     xmlHttp.open("GET", theUrl, true); // true for asynchronous
 
-    /* xmlHttp.onreadystatechange = () => {//func that check if the server over there is ready or not
+    xmlHttp.onreadystatechange = () => {//func that check if the server over there is ready or not
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
             //onreadystatechangeで「読み込み途中⁠」⁠，および「読み込み完了⁠」⁠・「⁠失敗」時の処理を行います。
             //まず，読み込みの状態を管理するのはreadyStateプロパティです。readyStateが4の時，読み込みの完了を示します。
             callback(xmlHttp.responseText);//read the text right here
-    } */
+    }
 
     xmlHttp.send(null);
-    return xmlHttp.responseText
 }
